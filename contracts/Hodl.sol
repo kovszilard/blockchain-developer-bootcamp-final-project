460e8d7c51e989d432d2455225c9a3cba9713380
Szilard Kovacs <kovszilard@gmail.com>
2021-11-30 23:33:08 +0100

    skeleton

---
+++ contracts/Hodl.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Hodl {
+  constructor() public {
+  }
+
+  function lockFunds(uint lockDurationDays) public payable {
+    //
+  }
+
+  function unlockFunds() public {
+    //
+  }
+
+  function registerHeritageAddress(address heritageAddress) public {
+    //
+  }
+
+  function ulockFundsWith(address heritageAddress) public {
+    //  
+  }
+
+  function calculatePenalty() private returns(uint) {
+    // 
+  }
+
+}
